f6cb9d2eb204fbb09ef4613b459f0d23260bad12
Anmolfid1 <87266877+Anmolfid1@users.noreply.github.com>
2022-10-16 21:53:43 +0530

    Update build.gradle.kts

--- NineAnimeProvider/build.gradle.kts
+++ NineAnimeProvider/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
         "OVA",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=9anime.id&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=9anime.gs&sz=%size%"
 }
